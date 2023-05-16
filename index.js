2c513886f7f20eae0d7489b6a5761da26f8c496a
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-05-15 21:37:25 -0400

    commit changes

---
+++ index.js
@@ -0,0 +1,58 @@
+// base/parent class
+class Vehicle {
+    constructor (model) {
+        this.model = model
+    }
+
+   drive() {
+        return `$(this.model) drives`
+    }
+}
+
+class Sedan extends Vehicle {
+    condtructor(model, backupCamera){
+        super(model)
+        this.model = model
+        this.backupCamera = backupCamera
+    }
+
+    drive() {
+        return `${this.model} drives`
+    }
+}
+
+class Suv extends Vehicle {
+    condtructor(model, offloadPackage){
+        super(model)
+        this.model = model
+        this.offroadPackagee = offroadPackage
+    }
+
+    drive() {
+        return `${this.model} drives`
+    }
+}
+
+class truck extends Vehicle {
+    condtructor(model, offloadPackage){
+        super(model)
+        this.model = model
+        this.towingCapacity = this.towingCapacity
+    }
+
+    drive() {
+        return `${this.model} drives`
+    }
+}
+
+class EVSadan  extends Vehicle {
+    condtructor(model, backupCamera){
+        super(model)
+        this.model = model
+        this.backupCamera = this.backupCamera  
+    }
+
+    drive() {
+        return `${this.model} drives`
+    }
+}
